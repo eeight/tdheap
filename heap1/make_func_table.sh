3448c3c784969649a6202f0bf160e9537afe50f7
Petr Prokhorenkov <prokhorenkov@gmail.com>
2009-03-01 15:26:48 +0300

    Modify function map creation code.

--- heap1/make_func_table.sh
+++ heap1/make_func_table.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-readelf -wi $1 | grep -E 'DW_AT_(name|(high|low)_pc)' | sed -re '
+readelf -wi $1 | grep -E '(DW_AT_(name|(high|low)_pc))|DW_TAG_subprogram' | sed -re '
+  s/.*DW_TAG_(subprogram).*/\1/
   s/DW_AT_//
   s/^\s*<[0-9a-z]+>\s*//
   s/\([^\)]+\)//
